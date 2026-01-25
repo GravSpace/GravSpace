4566470d59ea759df1ff46e638a0f71250320cf6
Rizal Muhamad H <rizalmuhamadh@gmail.com>
2026-01-25 18:49:56 +0700

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -17,4 +17,9 @@ export default defineNuxtConfig({
     componentDir: '@/components/ui'
   },
   devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8080',
+    },
+  },
 })
